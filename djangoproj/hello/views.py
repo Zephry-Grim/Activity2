d87f926f9c904709b1f55bdb761a949de6c987c1
Zephry-Grim <117063874+Zephry-Grim@users.noreply.github.com>
2022-11-09 12:36:54 +0800

    new commit

--- djangoproj/hello/views.py
+++ djangoproj/hello/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, BSIT")
+    return HttpResponse("Marloiz Seth Marcelino, BSIT-4B")
 # Create your views here.
